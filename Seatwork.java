dd34dda56d5615ae835e041c6836654d74a40358
antonethtam <antonethtam432@gmail.com>
2025-08-23 15:09:52 +0000

    Hi pls hurry

---
+++ Seatwork.java
@@ -0,0 +1,21 @@
+public class Seatwork{
+            public static void seatwork(String[] args) {
+                    int countdown = 3;
+                            while (countdown > 0) {
+                                        System.out.println(countdown);
+                                                    countdown--;
+                                                            }
+                                                                    System.out.println("Happy New Year!!");
+                                                                        }
+                                                                        }
+
+ 
+
+                       
+                                   
+                                              
+                                                        
+                                                               
+                                                                
+                                                    
+                                 
---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+        public static void Seatwork(String[] args) {
+                int countdown = 3;
+                        while (countdown > 0) {
+                                    System.out.println(countdown);
+                                                countdown--;
+                                                        }
+                                                                System.out.println("Happy New Year!!");
+                                                                    }
+                                                                    }
+
