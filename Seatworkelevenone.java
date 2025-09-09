45fa3f2f9597522b7f118f099f3c3a7c609ff22c
antonethtam <antonethtam432@gmail.com>
2025-09-09 10:18:47 +0000

    opo

---
+++ Seatworkelevenone.java
@@ -0,0 +1,15 @@
+public class Seatworkelevenone {
+
+    public static void main(String[] args) {
+
+    System.out.println("Seatworkelevenpartone");
+
+    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+
+    myNumbers [1][2] = 9;
+
+    System.out.println(myNumbers[1][2]);
+     // Outputs 9 Instead of 8
+
+     }
+     }
